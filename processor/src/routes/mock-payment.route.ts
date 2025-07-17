59c1275adcb9ac04f8e51d98b2c884cdefcde936
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-17 10:28:40 +0530

    Update mock-payment.route.ts

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -109,15 +109,11 @@ console.log('handle-novalnetResponse');
 
     },
   );
-
     fastify.get('/failure', async (request, reply) => {
     return reply.send('Payment was successful.');
   });
-	
-};
 
-//  Define the handler function separately
-export const handleRedirect = async (request: FastifyRequest, reply: FastifyReply) => {
+  fastify.get('/success', async (request: FastifyRequest, reply: FastifyReply) => {
   const query = request.query as {
     tid?: string;
     status?: string;
@@ -125,23 +121,26 @@ export const handleRedirect = async (request: FastifyRequest, reply: FastifyRepl
     txn_secret?: string;
   };
 
-  const paymentAccessKey = 'YTg3ZmY2NzlhMmYzZTcxZDkxODFhNjdiNzU0MjEyMmM=';
-
-  if (query.checksum && query.tid && query.status && query.txn_secret) {
-    const tokenString = `${query.tid}${query.txn_secret}${query.status}${paymentAccessKey}`;
+  const accessKey = 'YTg3ZmY2NzlhMmYzZTcxZDkxODFhNjdiNzU0MjEyMmM=';
+  if (query.tid && query.status && query.checksum && query.txn_secret) {
+    const tokenString = `${query.tid}${query.txn_secret}${query.status}${accessKey}`;
     const generatedChecksum = crypto.createHash('sha256').update(tokenString).digest('hex');
 
     if (generatedChecksum !== query.checksum) {
-      return reply.code(400).send('While redirecting some data has been changed. The hash check failed');
+      try {
+	const result = await opts.paymentService.createPaymentt({
+	  data: query.tid ?? '',
+	});
+	 return reply.code(400).send('redirect verifed');
+      } catch (error) {
+    	 return reply.code(400).send('Catch error failed');
+      }
     } else {
-      return reply.send('Payment redirect verified successfully.');
+      return reply.code(400).send('Checksum verification failed.');
     }
   } else {
-    return reply.send('Missing required query parameters.');
+    return reply.code(400).send('Missing required query parameters.');
   }
-};
+});
 
-//  Use this in your Fastify route setup
-export const registerRoutes = async (fastify: FastifyInstance) => {
-  fastify.get('/success', handleRedirect);
 };
