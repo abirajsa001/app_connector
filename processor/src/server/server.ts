f4d891f417c6989c70731105b90de9ca126fcf7f
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-17 15:35:00 +0530

    Update server.ts

--- processor/src/server/server.ts
+++ processor/src/server/server.ts
@@ -7,7 +7,6 @@ import { join } from 'path';
 import { config } from '../config/config';
 import { requestContextPlugin } from '../libs/fastify/context/context';
 import { errorHandler } from '../libs/fastify/error-handler';
-import { registerRoutes } from '../routes/mock-payment.route'; 
 
 export const setupFastify = async () => {
   const server = Fastify({
@@ -33,8 +32,6 @@ export const setupFastify = async () => {
   await server.register(autoLoad, {
     dir: join(__dirname, 'plugins'),
   });
-
-  await registerRoutes(server);
 
   return server;
 };
