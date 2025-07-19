95295fa7f386a4a62a8cbb7c676fc65554436d15
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-19 12:27:46 +0530

    Update mock-payment.route.ts

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -126,9 +126,6 @@ console.log('handle-novalnetResponse');
     const tokenString = `${query.tid}${query.txn_secret}${query.status}${accessKey}`;
     const generatedChecksum = crypto.createHash('sha256').update(tokenString).digest('hex');
     if (generatedChecksum !== query.checksum) {
-        const resp = await opts.paymentService.createPayments({
-          data: request.body,
-        });
 	return reply.code(400).send('redirect verifed');
     } else {
       return reply.code(400).send('Checksum verification failed.');
