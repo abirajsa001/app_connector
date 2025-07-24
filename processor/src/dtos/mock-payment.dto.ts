ba558f5bacc2d27a46b83f57a46e69aed487ae62
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-24 10:38:22 +0530

    Update mock-payment.dto.ts

--- processor/src/dtos/mock-payment.dto.ts
+++ processor/src/dtos/mock-payment.dto.ts
@@ -14,6 +14,7 @@ export enum PaymentMethodType {
 
 export const PaymentResponseSchema = Type.Object({
   paymentReference: Type.String(),
+  redirectUrl: Type.Optional(Type.String()),
 });
 console.log('mock-payment-dto.ts');
 export const PaymentOutcomeSchema = Type.Enum(PaymentOutcome);
