ccd5f42e026071d49ea8265ea78c5efbc26b60cd
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-28 10:28:35 +0530

    Update mock-payment.route.ts

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -136,8 +136,8 @@ console.log('handle-novalnetResponse');
           },
         });
 	
-	return reply.redirect(302, 'https://poc-novalnetpayments.frontend.site/en/thank-you/?orderId=c52dc5f2-f1ad-4e9c-9dc7-e60bf80d4a52');
-	 // return reply.code(400).send(result);
+	// return reply.redirect(302, 'https://poc-novalnetpayments.frontend.site/en/thank-you/?orderId=c52dc5f2-f1ad-4e9c-9dc7-e60bf80d4a52');
+	 return reply.code(400).send(result);
       } catch (error) {
     	 return reply.code(400).send('Catch error failed');
       }
@@ -168,8 +168,8 @@ fastify.get<{
     const resp = await opts.paymentService.createPayment({
       data: request.query,
     });
-	return reply.redirect(302, 'https://poc-novalnetpayments.frontend.site/en/thank-you/?orderId=c52dc5f2-f1ad-4e9c-9dc7-e60bf80d4a52');
-    // return reply.status(200).send(resp);
+	// return reply.redirect(302, 'https://poc-novalnetpayments.frontend.site/en/thank-you/?orderId=c52dc5f2-f1ad-4e9c-9dc7-e60bf80d4a52');
+    return reply.status(200).send(resp);
   }
 );
 
