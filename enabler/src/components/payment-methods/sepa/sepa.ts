1d345881aa17e6c1d8277cb9aff58f475a23241d
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-29 15:58:22 +0530

    Update sepa.ts

--- enabler/src/components/payment-methods/sepa/sepa.ts
+++ enabler/src/components/payment-methods/sepa/sepa.ts
@@ -88,30 +88,34 @@ export class Sepa extends BaseComponent {
     }
   }
 
-  private _getTemplate() {
-    const payButton = this.showPayButton
-      ? `<button class="${buttonStyles.button} ${buttonStyles.fullWidth} ${styles.submitButton}" id="purchaseOrderForm-paymentButton">Pay</button>`
+private _getTemplate() {
+  const payButton = this.showPayButton
+    ? `<button class="${buttonStyles.button} ${buttonStyles.fullWidth} ${styles.submitButton}" id="purchaseOrderForm-paymentButton">Pay</button>`
       : "";
-    return `
+
+  return `
     <div class="${styles.wrapper}">
-      <form class="${styles.paymentForm}">
-        <div class="${inputFieldStyles.inputContainer}">
-          <label class="${inputFieldStyles.inputLabel}" for="purchaseOrderForm-poNumber">
+      <form class="${styles.paymentForm}" id="purchaseOrderForm">
+        <div class="inputContainer">
+          <label class="inputLabel" for="purchaseOrderForm-poNumber">
             PO Number <span aria-hidden="true"> *</span>
           </label>
-          <input class="${inputFieldStyles.inputField}" type="text" id="purchaseOrderForm-poNumber" name="poNumber" value="">
-          <span class="${styles.hidden} ${inputFieldStyles.errorField}">Invalid PO number</span>
+          <input class="inputField" type="text" id="purchaseOrderForm-poNumber" name="poNumber" value="">
+          <span class="hidden errorField">Invalid PO number</span>
         </div>
-        <div class="${inputFieldStyles.inputContainer}">
-          <label class="${inputFieldStyles.inputLabel}" for="purchaseOrderForm-invoiceMemo">
+
+        <div class="inputContainer">
+          <label class="inputLabel" for="purchaseOrderForm-invoiceMemo">
             Invoice memo
           </label>
-          <input class="${inputFieldStyles.inputField}" type="text" id="purchaseOrderForm-invoiceMemo" name="invoiceMemo" value="">
-          <span class="${styles.hidden} ${inputFieldStyles.errorField}">Invalid Invoice memo</span>
+          <input class="inputField" type="text" id="purchaseOrderForm-invoiceMemo" name="invoiceMemo" value="">
+          <span class="hidden errorField">Invalid Invoice memo</span>
         </div>
+
         ${payButton}
       </form>
-      </div>
-    `;
-  }
+    </div>
+  `;
+}
+
 }
