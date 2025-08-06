b4ceb4b82a6cc13b01275d0cb7436c7016b548cb
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-08-06 13:11:40 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -296,6 +296,34 @@ public async createPaymentt({ data }: { data: any }) {
     body: JSON.stringify(novalnetPayload),
   });
   const responseData = await novalnetResponse.json();
+
+  const ctCart = await this.ctCartService.getCart({
+  	id: getCartIdFromContext(),
+  });
+	
+  const ctPayment = await this.ctPaymentService.createPayment({
+  amountPlanned: await this.ctCartService.getPaymentAmount({
+	cart: ctCart,
+  }),
+  paymentMethodInfo: {
+	paymentInterface: getPaymentInterfaceFromContext() || 'mock',
+  },
+  paymentStatus: { 
+	interfaceCode:  'TeXTCode',
+	interfaceText: 'TEXTFace',
+  },
+  ...(ctCart.customerId && {
+	customer: {
+	  typeId: 'customer',
+	  id: ctCart.customerId,
+	},
+  }),
+  ...(!ctCart.customerId &&
+	ctCart.anonymousId && {
+	  anonymousId: ctCart.anonymousId,
+	}),
+  });
+	
   return {
     success: parsedData ?? 'empty-response',
     novalnetResponse: responseData,
@@ -397,51 +425,51 @@ public async createPaymentt({ data }: { data: any }) {
 		Please use the following payment reference for your money transfer, as only through this way your payment is matched and assigned to the order:
 		Payment Reference 1: ${parsedResponse.transaction.tid}`;
 	}
-if(parsedResponse?.result?.redirect_url) {
-    const ctPayment = await this.ctPaymentService.createPayment({
-      amountPlanned: await this.ctCartService.getPaymentAmount({
-        cart: ctCart,
-      }),
-      paymentMethodInfo: {
-        paymentInterface: getPaymentInterfaceFromContext() || 'mock',
-      },
-    paymentStatus: { 
-        interfaceCode:  transactiondetails + '\n' + bankDetails,
-        interfaceText: responseString,
-      },
-      ...(ctCart.customerId && {
-        customer: {
-          typeId: 'customer',
-          id: ctCart.customerId,
-        },
-      }),
-      ...(!ctCart.customerId &&
-        ctCart.anonymousId && {
-          anonymousId: ctCart.anonymousId,
-        }),
-    });
 
-    await this.ctCartService.addPayment({
-      resource: {
-        id: ctCart.id,
-        version: ctCart.version,
-      },
-      paymentId: ctPayment.id,
-    });
+    // const ctPayment = await this.ctPaymentService.createPayment({
+    //   amountPlanned: await this.ctCartService.getPaymentAmount({
+    //     cart: ctCart,
+    //   }),
+    //   paymentMethodInfo: {
+    //     paymentInterface: getPaymentInterfaceFromContext() || 'mock',
+    //   },
+    // paymentStatus: { 
+    //     interfaceCode:  transactiondetails + '\n' + bankDetails,
+    //     interfaceText: responseString,
+    //   },
+    //   ...(ctCart.customerId && {
+    //     customer: {
+    //       typeId: 'customer',
+    //       id: ctCart.customerId,
+    //     },
+    //   }),
+    //   ...(!ctCart.customerId &&
+    //     ctCart.anonymousId && {
+    //       anonymousId: ctCart.anonymousId,
+    //     }),
+    // });
 
-    const pspReference = randomUUID().toString();
-    const updatedPayment = await this.ctPaymentService.updatePayment({
-      id: ctPayment.id,
-      pspReference: pspReference,
-      paymentMethod: request.data.paymentMethod.type,
-      transaction: {
-        type: 'Authorization',
-        amount: ctPayment.amountPlanned,
-        interactionId: pspReference,
-        state: this.convertPaymentResultCode(request.data.paymentOutcome),
-      },
-    });
-}
+    // await this.ctCartService.addPayment({
+    //   resource: {
+    //     id: ctCart.id,
+    //     version: ctCart.version,
+    //   },
+    //   paymentId: ctPayment.id,
+    // });
+
+    // const pspReference = randomUUID().toString();
+    // const updatedPayment = await this.ctPaymentService.updatePayment({
+    //   id: ctPayment.id,
+    //   pspReference: pspReference,
+    //   paymentMethod: request.data.paymentMethod.type,
+    //   transaction: {
+    //     type: 'Authorization',
+    //     amount: ctPayment.amountPlanned,
+    //     interactionId: pspReference,
+    //     state: this.convertPaymentResultCode(request.data.paymentOutcome),
+    //   },
+    // });
+
     return {
       // paymentReference: updatedPayment.id,
       paymentReference: parsedResponse?.result?.redirect_url ?? 'null',
