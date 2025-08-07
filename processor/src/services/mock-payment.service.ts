b6803f229dd6987adf8f07a544b041b28b17396d
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-08-07 11:09:15 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -558,6 +558,8 @@ public async createPaymentt({ data }: { data: any }) {
 	    inputval3: String(parsedCart.customerEmail ?? "Email not available"),
 	    input4: 'Payment-Method',
 	    inputval4: String(request.data.paymentMethod.type ?? "Payment-Method not available"), 
+            input5: 'order-number',
+	    inputval5: String(Context.getFutureOrderNumberFromContext() ?? 'order number not available'),
 	  }
 	};
 
