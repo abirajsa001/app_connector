697766d8456ac7e6c900f5ecd742dbafc9067247
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-08-12 12:36:33 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -331,7 +331,7 @@ public async createPaymentt(params: any) {
 		input1: 'currencyCode',
 		inputval1: JSON.stringify(parsedData),
 		input2: 'currencyCode',
-		inputval2: JSON.stringify(rawData),
+		inputval2: String(getCartIdFromContext() ?? "getCartIdFromContext not available"),
     }
   };
 
