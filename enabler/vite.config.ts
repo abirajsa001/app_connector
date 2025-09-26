ea2acd757803b3949a63489289dccfb2f34a6aa2
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-26 12:14:03 +0530

    Update vite.config.ts

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     }),
   ],
   build: {
-    outDir: resolve(__dirname, "public"),
+    outDir: resolve(__dirname, "dist"),
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/main.ts"),
