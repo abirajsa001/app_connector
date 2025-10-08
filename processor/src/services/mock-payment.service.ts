714b257a803ee746ecf031297b20d3091ef1828e
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-10-08 18:28:21 +0530

    test

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -124,7 +124,7 @@ export class MockPaymentService extends AbstractPaymentService {
               name: "Mock Payment API",
               status: "DOWN",
               message:
-                "The mock payment API is down for some reason. Please check the logs for more details test.",
+                "The mock payment API is down for some reason. Please check the logs for more details test test.",
               details: {
                 error: e,
               },
