f88ef699d0d97ef3715309fe9d931295f931ed1e
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-02-05 20:37:31 +0530

    update

--- processor/src/services/novalnet-payment.service.ts
+++ processor/src/services/novalnet-payment.service.ts
@@ -708,33 +708,6 @@ export class NovalnetPaymentService extends AbstractPaymentService {
 	const billingAddressStreetName = billingStreet.streetName;
 	const billingAddressStreetNumber = billingStreet.streetNumber;
 
-    log.warn(`[deliveryAddress] country=${billingAddress}, result=${deliveryAddress}, deliveryAddressStreetName=${deliveryAddressStreetName}, deliveryAddressStreetNumber=${deliveryAddressStreetNumber}`);
-    log.warn(`[billingAddress] country=${billingAddress}, result=${deliveryAddress}, billingAddressStreetName=${billingAddressStreetName}, billingAddressStreetNumber=${billingAddressStreetNumber}`);
-    log.warn(
-      `[billingAddressJSON] country=${billingAddress?.country}, address=${JSON.stringify(billingAddress)}`
-    );
-    
-    log.warn(
-      `[deliveryAddressJSON] country=${deliveryAddress?.country}, address=${JSON.stringify(deliveryAddress)}`
-    );
-    
-    log.warn("[billingAddressJSONLogin]", {
-      country: billingAddress?.country,
-      address: billingAddress,
-    });
-    
-    log.warn("[deliveryAddressJSONLogin]", {
-      country: deliveryAddress?.country,
-      address: deliveryAddress,
-    });
-    log.warn(
-      `[billingAddressJSONPretty]\n${JSON.stringify(billingAddress, null, 2)}`
-    );
-
-    log.warn(
-      `[deliveryAddressJSONPretty]\n${JSON.stringify(deliveryAddress, null, 2)}`
-    );
-
     if (dueDateValue) {
       transaction.due_date = dueDateValue;
     }
