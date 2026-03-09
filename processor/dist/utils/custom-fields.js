3e6e1a83cca1c129c553ee010921a45d6933d0db
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-03-09 15:53:31 +0530

    payment-transaction

--- processor/dist/utils/custom-fields.js
+++ processor/dist/utils/custom-fields.js
@@ -37,7 +37,7 @@ const createTransactionCommentsType = async () => {
                 body: {
                     key: "novalnet-transaction-comments",
                     name: { en: "Novalnet Transaction Comments" },
-                    resourceTypeIds: ["payment"],
+                    resourceTypeIds: ["transaction"],
                     fieldDefinitions: [
                         {
                             name: "transactionComments",
